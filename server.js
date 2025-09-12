8f81065b234a6c7cc320c824435fdd1a73ad6cfd
Ruqaya Husain <ruqayanader22@gmail.com>
2025-09-12 20:11:05 +0300

    show, edit, and delete functionality

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const morgan = require('morgan');
 const mongoose =  require('mongoose');
+const methodOverride = require('method-override');
 
 const app = express();
 const PORT = 3000;
@@ -15,6 +16,7 @@ mongoose.connection.on('connected', () => {
 const Planet = require('./models/planet');// you don't have to write the file's extension (.js), when using the require function
 
 // MIDDLEWARE
+app.use(methodOverride('_method')); // we override the method before logging it using morgan
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false}));// a middleware that converts model into a javascript object
 
@@ -43,6 +45,38 @@ app.post('/planets', async (req, res) => {
   await Planet.create(req.body); // creates a record
   res.redirect('/planets');
 });
+
+app.get('/planets/:planetId', async (req, res) => { // displays a specific plant by its ID
+  const planetId = req.params.planetId;
+  const planet = await Planet.findById(planetId); // get planet from DB base on the id passed in the parameter
+  res.render('planets/show.ejs', { planet });
+});
+
+app.get('/planets/:planetId/edit', async (req, res) => { // displays a specific plant by its ID
+  const planetId = req.params.planetId;
+  const planet = await Planet.findById(planetId); // get planet from DB base on the id passed in the parameter
+  res.render('planets/edit.ejs', { planet });
+});
+
+app.put('/planets/:planetId', async (req, res) => { // updates a specific plant by its ID
+  const planetId = req.params.planetId;
+
+  if(req.body.hasRings === 'on') {
+    req.body.hasRings = true;
+  } else {
+    req.body.hasRings = false;
+  }
+
+  await Planet.findByIdAndUpdate(planetId, req.body); // updated the specific planet
+
+  res.redirect(`/planets/${planetId}`); // redirects user to the show page
+});
+
+app.delete('/planets/:planetId', async (req, res) => { // deletes a specific plant by its ID
+  const planetId = req.params.planetId;
+  const planet = await Planet.findByIdAndDelete(planetId);
+  res.redirect('/planets');
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
